934285edf6de2e23fb699a77aea400680eec3a3f
Amr <amr.effat2011@gmail.com>
2023-05-03 14:17:57 +0300

    i'll doit

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,36 @@ package main
 
 import (
 	"fmt"
+	"hex/internal/adapters/app/api"
 	"hex/internal/adapters/core/arithmetic"
+	gRBC "hex/internal/adapters/framework/left/grpc"
+	"hex/internal/adapters/framework/right/db"
+	"hex/internal/ports"
+	"log"
+	"os"
 	//"hex/internal/ports"
 )
 
 func main() {
+	var err error
+	var dbaseAdapter ports.DbPort
+	var core ports.ArithmeticPort
+	var appAdapter ports.APIPort
+	var gRPCAdapter ports.GRPCPorts
 
+	dbaseDriver := os.Getenv("DB_DRIVER")
+	dbsourceName := os.Getenv("DS_Name")
+
+	dbaseAdapter, err = db.NewAdapter(dbaseDriver, dbsourceName)
+
+	if err != nil {
+		log.Fatalf("error creating db adapter %v", err)
+	}
+	defer dbaseAdapter.CloseDbConnection()
+	core = arithmetic.NewAdapter()
+	appAdapter = api.NewAdapter(dbaseAdapter, core)
+	gRPCAdapter = gRBC.NewAdapter(appAdapter)
+	gRPCAdapter.Run()
 	// ports
 	// var core ports.ArithmeticPort
 	// core = arithmetic.NewAdapter()
